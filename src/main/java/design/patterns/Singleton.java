f8e0ac974e65a94495e02c6e9dd4331d636d1cfb
lys <10021071@qq.com>
2018-08-22 15:46:56 +0800

    添加拼音工具

---
+++ src/main/java/design/patterns/Singleton.java
@@ -0,0 +1,38 @@
+package design.patterns;
+
+/**
+ * lys
+ * <p>
+ * 2018年1月11日  上午11:07:53
+ */
+public enum Singleton {
+    INSTANCE;
+    private A a;
+
+    Singleton() {
+        a = new A();
+        System.out.println("枚举私有的构造方法");
+    }
+
+    public A getInstance() {
+        return a;
+    }
+
+}
+
+/**
+ * 需要单例的类
+ * <p>
+ * lys
+ * <p>
+ * 2018年1月11日  上午11:10:05
+ */
+class A {
+    public static void main(String[] args) {
+        Singleton.INSTANCE.getInstance();
+        Singleton.INSTANCE.getInstance();
+        Singleton.INSTANCE.getInstance();
+    }
+}
+
+
---
+++ src/main/java/utils/PinyinUtil.java
@@ -0,0 +1,29 @@
+package utils;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+
+public class PinyinUtil {
+
+    // 返回中文的首字母
+    public static String getPinYinHeadChar(String str){
+        String convert = "";
+        int len = str.length();
+        for(int j = 0;j < len; j++){
+            char word = str.charAt(j);
+            String[] pinyinArray = PinyinHelper.toHanyuPinyinStringArray(word);
+            if (pinyinArray != null) {
+                convert += pinyinArray[0].charAt(0);
+            } else {
+                convert += word;
+            }
+            break;
+        }
+        return convert.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getPinYinHeadChar("大伦"));
+
+    }
+
+}
