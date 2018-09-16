6a7f0ecdfd990e7cb65645483f934c3d3a7d996c
lys <10021071@qq.com>
2018-09-16 18:00:07 +0800

    更新

--- md/linux命令.md
+++ md/linux命令.md
@@ -10,6 +10,9 @@ firewall-cmd --zone=public --add-port=80/tcp --permanent
 重启防火墙
 firewall-cmd  --reload
 
+### 关于硬盘空间的命令
+`df -lh`
+
 nohup java -jar xxx.jar&命令，则程序会在后台一直运行，
 
 
--- src/main/java/com/controller/TestFtlController.java
+++ src/main/java/com/controller/TestFtlController.java
@@ -2,6 +2,7 @@ package com.controller;
 
 import com.utils.IpUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,9 @@ public class TestFtlController {
     @Autowired
     private Resource resource;
 
+    @Value("${server.port}")
+    private String port;
+
     @RequestMapping("studyIndex")
     public String studyIndex(ModelMap modelMap) {
         modelMap.addAttribute("resource",resource);
@@ -20,7 +24,7 @@ public class TestFtlController {
 
     @RequestMapping("index")
     public String index(ModelMap modelMap) {
-        modelMap.addAttribute("ip", IpUtils.getOuterNetIp());
+        modelMap.addAttribute("ip", IpUtils.getOuterNetIp()+":"+port);
         return "ip";
     }
 
