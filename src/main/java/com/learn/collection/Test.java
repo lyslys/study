0a475860107e13b49223ebacc3bd579363e26404
lys <10021071@qq.com>
2018-10-23 10:51:41 +0800

    常用工具

--- src/main/java/com/learn/collection/Test.java
+++ src/main/java/com/learn/collection/Test.java
@@ -1,5 +1,14 @@
 package com.learn.collection;
 
+
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.*;
 
 public class Test {
@@ -19,5 +28,35 @@ public class Test {
         System.out.println(collection2.size());
         System.out.println(collection2);
 
+        System.out.println("--------------");
+
+        List<Integer> integerList = new LinkedList<>();
+        List<Integer> integerList2 = null;
+//        for(Integer in:integerList2){
+//            System.out.println(in);
+//        }
+
+        aa();
+
+    }
+
+    public static void aa(){
+        try {
+
+            File file = new File("D:/home/kyapp/uploadfile/20181022/cc.xls");
+       //     file.createNewFile();
+                Workbook wb = Workbook.getWorkbook(file);
+               System.out.println(wb.getSheet(0));
+            WritableWorkbook book = Workbook.createWorkbook(file);
+            WritableSheet sheet = book.createSheet("sheet11", 0);
+//                WritableWorkbook wwb = Workbook.createWorkbook(new FileOutputStream("E:\\b.xls"));
+//                //创建第一个工作表对象,名为("Sheet 1)
+//                WritableSheet ws = wwb.createSheet("Sheet 1", 0);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }
     }
 }
