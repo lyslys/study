78bc5559c8e090865df53c304d955aa133f88b05
yan <10021071@qq.com>
2022-04-12 19:32:51 +0800

    添加lua

--- src/main/java/com/utils/CreatePasswd.java
+++ src/main/java/com/utils/CreatePasswd.java
@@ -25,9 +25,36 @@ public class CreatePasswd {
         return sb.toString();
     }
 
+    //生成随机用户名，数字和字母组成,
+    public static String getRandomString(int length) {
+
+        String val = "";
+        Random random = new Random();
+
+        //参数length，表示生成几位随机数
+        for (int i = 0; i < length; i++) {
+
+            String charOrNum = random.nextInt(2) % 2 == 0 ? "char" : "num";
+            //输出字母还是数字
+            if ("char".equalsIgnoreCase(charOrNum)) {
+                //输出是大写字母还是小写字母
+                int temp = random.nextInt(2) % 2 == 0 ? 65 : 97;
+                val += (char) (random.nextInt(26) + temp);
+            } else {
+                int n;
+                do{
+                   n =  random.nextInt(10);
+                }while (n == 4);
+                val += String.valueOf(n);
+            }
+        }
+        return val;
+    }
+
     public static void main(String[] args) {
-        System.out.println(getRandomPassword(16));
+        System.out.println(getRandomPassword(8));
         System.out.println((int)((Math.random()*9+1)*10000));
+        System.out.println(getRandomString(6));
     }
 
 }
